4fd85052a7aa1e1d84ac3b2284c96da2d2ddc2c0
SinOfGuardian <99592583+SinOfGuardian@users.noreply.github.com>
2022-02-25 12:31:05 +0800

    connect

---
+++ app/src/main/java/com/example/aredoweknow/ConnectionMYSQL.java
@@ -0,0 +1,10 @@
+package com.example.aredoweknow;
+
+public class ConnectionMYSQL {
+    String JavaCon ="com.mysql.jdbc.Driver";
+
+    String url ="";
+    String name ="";
+    String Passwd ="";
+
+}
