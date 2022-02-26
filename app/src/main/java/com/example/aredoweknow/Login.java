8ded60234ca095d9eaeef8674a58bffe7d7fb45e
Jerreme De Torres <jerremedetorres029@gmail.com>
2022-02-26 15:22:55 +0800

    UI Updated
    - Login Views Size slightly changed, like fonts, height
    - Margins Increased to make them separated evenly
    - Titles set Color set to gradient, but will be remove later when  our logo comes up
    - Slpashscreen background changed to Gradient

--- app/src/main/java/com/example/aredoweknow/Login.java
+++ app/src/main/java/com/example/aredoweknow/Login.java
@@ -3,8 +3,10 @@ package com.example.aredoweknow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -12,6 +14,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        -----------------This sets the Title to Gradient Color-----------------
+        TextView title = findViewById(R.id.Title);
+        title.setTextColor(getResources().getColor(R.color.gr1_end));
+        Shader textShader = new LinearGradient(0, 0, title.getPaint().measureText(title.getText().toString()), title.getTextSize(),
+                new int[]{getResources().getColor(R.color.gr1_end),
+                        getResources().getColor(R.color.gr1_start)},
+                null, Shader.TileMode.MIRROR);
+        title.getPaint().setShader(textShader); //--> paint to gradient
+//        ------------------------------------------------------------------------
+
     }
 
     public void signupBtn_clicked(View view) {
--- app/src/main/java/com/example/aredoweknow/Register.java
+++ app/src/main/java/com/example/aredoweknow/Register.java
@@ -2,8 +2,11 @@ package com.example.aredoweknow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
@@ -11,6 +14,18 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+//        -----------------This sets the Title to Gradient Color-----------------
+        TextView title = findViewById(R.id.Title);
+        title.setTextColor(getResources().getColor(R.color.gr1_end));
+        Shader textShader = new LinearGradient(0, 0, title.getPaint().measureText(title.getText().toString()), title.getTextSize(),
+                new int[]{getResources().getColor(R.color.gr1_end),
+                        getResources().getColor(R.color.gr1_start)},
+                null, Shader.TileMode.MIRROR);
+        title.getPaint().setShader(textShader); //--> paint to gradient
+//        ------------------------------------------------------------------------
+
     }
 
     public void signin2Btn_clicked(View view) {
