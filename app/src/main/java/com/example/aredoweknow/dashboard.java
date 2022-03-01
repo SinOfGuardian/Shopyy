62e883423a4d6ceb2c37e33d229c650881702fff
SinOfGuardian <99592583+SinOfGuardian@users.noreply.github.com>
2022-03-01 15:22:28 +0800

    Dashboard adding error
    -dashboard textfield center
    -Adding string for navigation
    -adding menu for navigation menu
    -.java for dashboard
    -dashboard
    -RUNTIME ERROR

--- app/src/main/java/com/example/aredoweknow/dashboard.java
+++ app/src/main/java/com/example/aredoweknow/dashboard.java
@@ -1,16 +1,47 @@
 package com.example.aredoweknow;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class dashboard extends AppCompatActivity {
+    public class dashboard extends AppCompatActivity {
 
+        public DrawerLayout drawerLayout;
+        public ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-}
+
+        // override the onOptionsItemSelected()
+        // function to implement
+        // the item click listener callback
+        // to open and close the navigation
+        // drawer when the icon is clicked
+        @Override
+        public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+            if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
