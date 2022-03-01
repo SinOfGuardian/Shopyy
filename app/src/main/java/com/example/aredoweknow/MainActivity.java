ac98a8182746779bd4e5b38a0df0aa657f2dc439
Jerreme De Torres <jerremedetorres029@gmail.com>
2022-03-01 18:24:42 +0800

    FAB Added and WebView Function
    - FAB Buttons Added
    - WebView Features
    - Icon Animation in SplashScreen!

--- app/src/main/java/com/example/aredoweknow/MainActivity.java
+++ app/src/main/java/com/example/aredoweknow/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aredoweknow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -9,6 +10,8 @@ import android.os.Handler;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,27 +21,36 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Login;
-    private TextView register1;
+    private Animation bounce;
+    private AppCompatImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        logo = findViewById(R.id.logo);
+        bounce = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.boucne_anim);
+
+        //-------------This is the Bounce Animation------------
         Handler handler = new Handler();
+        handler.postAtTime(new Runnable() {
+            @Override
+            public void run() {
+                logo.startAnimation(bounce);
+            }
+        }, 100);
+        //------------------------------------------------------
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent activity = new Intent(MainActivity.this, Login.class);
+//                Intent activity = new Intent(MainActivity.this, Login.class);
+                Intent activity = new Intent(MainActivity.this, dashboard.class);
                 startActivity((Intent) activity); // --> Start Login Activity
                 finish(); // --> prevent user from coming back to this activity
             }
         }, 2000);
 
-
-
     }
-
-
-
 }
--- app/src/main/java/com/example/aredoweknow/dashboard.java
+++ app/src/main/java/com/example/aredoweknow/dashboard.java
@@ -1,16 +1,32 @@
 package com.example.aredoweknow;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class dashboard extends AppCompatActivity {
+    FloatingActionButton browser_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        browser_btn = findViewById(R.id.browser_button);
+
+
+        //Open Web View
+        browser_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dashboard.this, Web.class);
+                startActivity(intent);
+            }
+        });
     }
 }
